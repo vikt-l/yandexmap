caceec908cde474b3d7b30d89b5a7eee3413de19
vikt-l <viktorialesnyh87@gmail.com>
2023-01-23 23:23:32 +0300

    added check_index, try-except

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel, QPushButton, QCheckBox
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from io import BytesIO
@@ -77,150 +77,73 @@ class Form(QMainWindow):
         self.btn_reset.move(410, 70)
         self.btn_reset.clicked.connect(self.reset)
 
+        self.cbox1 = QCheckBox(self)
+        self.cbox1.move(0, 0)
+        self.cbox1.setChecked(False)
+        self.cbox1.toggled.connect(self.check_index)
+
+    def check_index(self):
+        try:
+            if self.cbox1.isChecked():
+                self.lbl.hide()
+
+                try:
+                    self.delta = str(float(self.mashtab.text()))
+                except Exception:
+                    self.delta = '0.005'
+
+                toponym_to_find = self.address.text()
+                geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+                geocoder_params = {
+                    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                    "geocode": toponym_to_find,
+                    "format": "json"}
+
+                response = requests.get(geocoder_api_server, params=geocoder_params)
+
+                if not response:
+                    pass
+                json_response = response.json()
+                toponym = json_response["response"]["GeoObjectCollection"][
+                    "featureMember"][0]["GeoObject"]
+                self.toponym_index = toponym["metaDataProperty"]["GeocoderMetaData"]['Address']['postal_code']
+
+                self.address.setText(self.address.text() + ', ' + self.toponym_index)
+
+        except Exception:
+            pass
+
     def reset(self):
         self.address.setText('Введите адрес')
         self.object.setText('Введите запрос')
         self.get_info()
 
-
     def get_info(self):
 
-        self.lbl.hide()
-
-        self.toponym_longitude = self.coord_x.text()
-        self.toponym_lattitude = self.coord_y.text()
-        self.delta = self.mashtab.text()
-
         try:
-            self.delta = str(float(self.mashtab.text()))
-        except Exception:
-            self.delta = '0.005'
+            self.lbl.hide()
 
-        try:
-            self.toponym_longitude = str(float(self.toponym_longitude))
-            self.toponym_lattitude = str(float(self.toponym_lattitude))
+            self.toponym_longitude = self.coord_x.text()
+            self.toponym_lattitude = self.coord_y.text()
+            self.delta = self.mashtab.text()
 
-        except Exception:
-            self.lbl.show()
-            return None
+            try:
+                self.delta = str(float(self.mashtab.text()))
+            except Exception:
+                self.delta = '0.005'
 
-        map_params = {
-            "ll": ",".join([self.toponym_longitude, self.toponym_lattitude]),
-            "spn": ",".join([self.delta, self.delta]),
-            "l": "map"
-        }
+            try:
+                self.toponym_longitude = str(float(self.toponym_longitude))
+                self.toponym_lattitude = str(float(self.toponym_lattitude))
 
-        map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(map_api_server, params=map_params)
-
-        im = Image.open(BytesIO(response.content))
-        im.save('img.png')
-        self.pixmap = QPixmap('img.png')
-        self.pixmap = self.pixmap.scaled(730, 600, Qt.KeepAspectRatio)
-        self.image.setPixmap(self.pixmap)
-
-    def get_address(self):
-
-        self.lbl.hide()
-
-        try:
-            self.delta = str(float(self.mashtab.text()))
-        except Exception:
-            self.delta = '0.005'
-
-        toponym_to_find = self.address.text()
-        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-        geocoder_params = {
-            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "geocode": toponym_to_find,
-            "format": "json"}
-
-        response = requests.get(geocoder_api_server, params=geocoder_params)
-
-        if not response:
-            pass
-        json_response = response.json()
-        toponym = json_response["response"]["GeoObjectCollection"][
-            "featureMember"][0]["GeoObject"]
-        toponym_coodrinates = toponym["Point"]["pos"]
-        toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-
-        map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
-            "spn": ",".join([self.delta, self.delta]),
-            "l": "map"
-        }
-
-        self.coord_x.setText(toponym_longitude)
-        self.coord_y.setText(toponym_lattitude)
-        self.mashtab.setText(self.delta)
-
-        map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(map_api_server, params=map_params)
-
-        im = Image.open(BytesIO(response.content))
-        im.save('img.png')
-        self.pixmap = QPixmap('img.png')
-        self.pixmap = self.pixmap.scaled(730, 600, Qt.KeepAspectRatio)
-        self.image.setPixmap(self.pixmap)
-
-    def get_obj(self):
-
-        self.lbl.hide()
-
-        self.toponym_longitude = self.coord_x.text()
-        self.toponym_lattitude = self.coord_y.text()
-        self.delta = self.mashtab.text()
-
-        try:
-            self.delta = str(float(self.mashtab.text()))
-        except Exception:
-            self.delta = '0.005'
-
-        try:
-            self.toponym_longitude = str(float(self.toponym_longitude))
-            self.toponym_lattitude = str(float(self.toponym_lattitude))
-
-            self.coord_x.setText(self.toponym_longitude)
-            self.coord_y.setText(self.toponym_lattitude)
-            self.mashtab.setText(self.delta)
-
-        except Exception:
-            self.lbl.show()
-            return None
-
-        search_api_server = "https://search-maps.yandex.ru/v1/"
-        api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-
-        address_ll = ",".join([self.toponym_longitude, self.toponym_lattitude])
-
-        search_params = {
-            "apikey": api_key,
-            "text": self.object.text(),
-            "lang": "ru_RU",
-            "ll": address_ll,
-            "type": "biz"
-        }
-
-        response = requests.get(search_api_server, params=search_params)
-
-        if response:
-            json_response = response.json()
-
-            organization = json_response["features"][0]
-            org_name = organization["properties"]["CompanyMetaData"]["name"]
-            org_address = organization["properties"]["CompanyMetaData"]["address"]
-
-            self.address.setText(org_address)
-
-            point = organization["geometry"]["coordinates"]
-            org_point = "{0},{1}".format(point[0], point[1])
+            except Exception:
+                self.lbl.show()
+                return None
 
             map_params = {
-                "ll": org_point,
+                "ll": ",".join([self.toponym_longitude, self.toponym_lattitude]),
                 "spn": ",".join([self.delta, self.delta]),
-                "l": "map",
-                "pt": "{0},pm2dgl".format(org_point)
+                "l": "map"
             }
 
             map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -231,6 +154,134 @@ class Form(QMainWindow):
             self.pixmap = QPixmap('img.png')
             self.pixmap = self.pixmap.scaled(730, 600, Qt.KeepAspectRatio)
             self.image.setPixmap(self.pixmap)
+
+            self.cbox1.setChecked(False)
+
+        except Exception:
+            pass
+
+    def get_address(self):
+        try:
+            self.lbl.hide()
+
+            try:
+                self.delta = str(float(self.mashtab.text()))
+            except Exception:
+                self.delta = '0.005'
+
+            toponym_to_find = self.address.text()
+            geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+            geocoder_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "geocode": toponym_to_find,
+                "format": "json"}
+
+            response = requests.get(geocoder_api_server, params=geocoder_params)
+
+            if not response:
+                pass
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"][
+                "featureMember"][0]["GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+            self.toponym_index = toponym["metaDataProperty"]["GeocoderMetaData"]['Address']['postal_code']
+
+            map_params = {
+                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                "spn": ",".join([self.delta, self.delta]),
+                "l": "map"
+            }
+
+            self.coord_x.setText(toponym_longitude)
+            self.coord_y.setText(toponym_lattitude)
+            self.mashtab.setText(self.delta)
+
+            map_api_server = "http://static-maps.yandex.ru/1.x/"
+            response = requests.get(map_api_server, params=map_params)
+
+            im = Image.open(BytesIO(response.content))
+            im.save('img.png')
+            self.pixmap = QPixmap('img.png')
+            self.pixmap = self.pixmap.scaled(730, 600, Qt.KeepAspectRatio)
+            self.image.setPixmap(self.pixmap)
+
+            self.check_index()
+        except Exception:
+            pass
+
+    def get_obj(self):
+        try:
+            self.lbl.hide()
+
+            self.toponym_longitude = self.coord_x.text()
+            self.toponym_lattitude = self.coord_y.text()
+            self.delta = self.mashtab.text()
+
+            try:
+                self.delta = str(float(self.mashtab.text()))
+            except Exception:
+                self.delta = '0.005'
+
+            try:
+                self.toponym_longitude = str(float(self.toponym_longitude))
+                self.toponym_lattitude = str(float(self.toponym_lattitude))
+
+                self.coord_x.setText(self.toponym_longitude)
+                self.coord_y.setText(self.toponym_lattitude)
+                self.mashtab.setText(self.delta)
+
+            except Exception:
+                self.lbl.show()
+                return None
+
+            search_api_server = "https://search-maps.yandex.ru/v1/"
+            api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+            address_ll = ",".join([self.toponym_longitude, self.toponym_lattitude])
+
+            search_params = {
+                "apikey": api_key,
+                "text": self.object.text(),
+                "lang": "ru_RU",
+                "ll": address_ll,
+                "type": "biz"
+            }
+
+            response = requests.get(search_api_server, params=search_params)
+
+            if response:
+                json_response = response.json()
+
+                organization = json_response["features"][0]
+                org_name = organization["properties"]["CompanyMetaData"]["name"]
+                org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+                self.address.setText(org_address)
+
+                point = organization["geometry"]["coordinates"]
+                org_point = "{0},{1}".format(point[0], point[1])
+
+                map_params = {
+                    "ll": org_point,
+                    "spn": ",".join([self.delta, self.delta]),
+                    "l": "map",
+                    "pt": "{0},pm2dgl".format(org_point)
+                }
+
+                map_api_server = "http://static-maps.yandex.ru/1.x/"
+                response = requests.get(map_api_server, params=map_params)
+
+                im = Image.open(BytesIO(response.content))
+                im.save('img.png')
+                self.pixmap = QPixmap('img.png')
+                self.pixmap = self.pixmap.scaled(730, 600, Qt.KeepAspectRatio)
+                self.image.setPixmap(self.pixmap)
+
+                self.check_index()
+
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
